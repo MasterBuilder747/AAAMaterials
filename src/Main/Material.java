facd22d9134e8942aafd9e9e69436fba6fbd1311
Joseph <jaathunder@yahoo.com>
2021-05-25 00:32:27 -0700

    finished planning for material build

--- src/Main/Material.java
+++ src/Main/Material.java
@@ -4,32 +4,74 @@ public class Material {
     String name;
     String localizedName;
     int color;
+    String formula; //toolTip
+
+    //every material is a dust material!
+    boolean ore;
+    boolean gem;
+    boolean smelt;
+    boolean conductive; //requires smelt
+    boolean blast; //requires smelt
 
     //only if compound is true
+    //compounds include different materials, not multiple of just 1! ex: H2 gas is NOT a compound
     //f = physical, t = chemical
     boolean compound = false; //alloy, gem, mineral, ore dust
+    int separation; //2 = chemical, 1 = physical separation/combination, 0 is none
+    int combination; //2 = chemical, 1 = physical separation/combination, 0 is none
+    String composition; //a string of defined element(s) and their count(s) in a string with special syntax
 
-    int separation; //-1 = chemical, 1 = physical separation/combination, 0 is none
-    int combination; //-1 = chemical, 1 = physical separation/combination, 0 is none
-
-    boolean conductive = false;
-    boolean magnetic = false;
-
+    //1) set basic info
     public Material(String name, String localizedName, int color) {
         this.name = name;
         this.localizedName = localizedName;
         this.color = color;
     }
-
-
-
+    //2) element or compound? Cannot be both
+    public void setElement(String composition) {
+        this.composition = composition;
+    }
+    public void setCompound(String composition, int separation, int combination) {
+        this.compound = true;
+        this.composition = composition;
+        this.separation = separation;
+        this.combination = combination;
+    }
+    //3) set attributes (what parts should generate?)
+    public void ore() {
+        this.ore = true;
+    }
+    public void gem() {
+        this.gem = true;
+    }
+    public void smelt() {
+        this.smelt = true;
+    }
+    public void conductive() {
+        this.conductive = true;
+    }
+    public void blast() {
+        this.blast = true;
+    }
+    //4) build the code based off these attributes
+    public String buildBuilder() {
+        //1) build the material
+        return "var " + this.name + " = MaterialSystem.getMaterialBuilder().setName(\"" + this.localizedName + "\").setColor(" + this.color + ").build();\n";
+    }
     public String buildMaterial() {
         StringBuilder sb = new StringBuilder();
-
+        //make sure the vars are defined since they are in the same scope
+        //2) generate parts, ores, blocks (get variable arrays)
+        //Material.registerParts(ore)
+        //Material.registerParts(dust, etc...)
+        //var ores = Material.registerParts(ore_types);
+        //for i, ore in ores {
+        //  var oreData = ore.getData();
+        //  //build OreVariant here
+        //}
+        //var blockData = Material.registerPart("block").getData();
+        //3) generate toolTip based of the composition string
         return sb.toString();
-    }
-    public String buildBuilder() {
-        return "var " + this.name + " = MaterialSystem.getMaterialBuilder().setName(\"" + this.localizedName + "\").setColor(" + this.color + ").build();\n";
     }
 
 }
