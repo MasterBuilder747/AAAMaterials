270fdda495c41c74eb3b9d34b338fcac5a0d5d7b
MasterBuilder747 <jaathunder@gmail.com>
2022-09-22 08:50:39 -0700

    code cleanup old classes

--- src/Main/Data/RecipeObject/Material/Composition/CompoundComposition.java
+++ src/Main/Data/RecipeObject/Material/Composition/CompoundComposition.java
@@ -73,4 +73,33 @@ public class CompoundComposition extends AChemicalComposition {
     protected String buildSpecificRecipe() {
         return null;
     }
+
+    //composition definition that contains multiple molecule materials in it
+/*
+
+    public Composition createCompound(String s) {
+        //[molecule1; molecule2*2; molecule3 * 3]
+        String s1 = s.substring(1, s.length()-1);
+        String[] moles = Util.split(s1, ";");
+
+        ArrayList<Composition> comps = new ArrayList<>();
+//        for (String name : moles) {
+//            if (name.contains("*")) {
+//                if (mol.is(name.substring(0, name.indexOf("*")))) {
+//                    comps.add(new Composition("", mol.get(name.substring(0, name.indexOf("*"))), Integer.parseInt(name.substring(name.indexOf("*")+1))));
+//                } else {
+//                    error("Unknown material " + name.substring(0, name.indexOf("*")));
+//                }
+//            } else {
+//                if (mol.is(name)) {
+//                    comps.add(new Composition("", mol.get(name)));
+//                } else {
+//                    error("Unknown material " + name);
+//                }
+//            }
+//        }
+        return buildComposition(comps);
+    }
+
+ */
 }
